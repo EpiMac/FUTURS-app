b87c546d6230c662744672a4478f5bfc089f166e
Tom-Eliott Herfray <tomeliott@icloud.com>
2021-09-29 13:01:38 +0200

    [ADD] Adding a ListView with a Company object

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,35 @@ void main() {
   ));
 }
 
+class Company {
+  int id;
+  String name;
+  String description;
+  IconData icon;
+  String photo;
+
+  Company(this.id,
+          this.name,
+          this.description,
+          this.icon,
+          this.photo);
+}
+
+// TESTS (without API call)
+Company test1 = new Company(1,
+                            "Microsoft",
+                            "It's Microsoft bro",
+                            Icons.ac_unit,
+                            "https://pbs.twimg.com/profile_images/1234407307955535873/0pjqJnab_400x400.png");
+
+Company test2 = new Company(2,
+                            "Ubisoft",
+                            "Assassin's Creed for Life",
+                            Icons.access_alarm,
+                            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6P_xCU5PeM3YF4MPc2e5IbRCrPyeoj1iGYln1Zgaklvyi0jbXyozuTQ7oew78cCAkFzU&usqp=CAU");
+
+final companies = [test1, test2];
+
 class Futurs_HomeRoute extends StatelessWidget {
   const Futurs_HomeRoute({Key? key}) : super(key: key);
 
@@ -18,7 +47,7 @@ class Futurs_HomeRoute extends StatelessWidget {
       ),
       body: Center(
         child: ElevatedButton(
-          child: const Text('Open route'),
+          child: const Text('Discover companies'),
           onPressed: () {
             Navigator.push(
               context,
@@ -36,18 +65,17 @@ class Futurs_CompaniesRoute extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Companies Route"),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: const Text('Go back!'),
-        ),
-      ),
-    );
+    return ListView.builder(
+        itemCount: companies.length,
+        itemBuilder: (context, index) {
+          return Card(
+              child: ListTile(
+                  title: Text(companies[index].name),
+                  subtitle: Text(companies[index].description),
+                  leading: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          companies[index].photo)),
+                  trailing: Icon(companies[index].icon)));
+        });
   }
 }
